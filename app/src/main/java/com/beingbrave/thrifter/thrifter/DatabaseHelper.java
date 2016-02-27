03af36c5934e0d4ad177ae3e30b3c4ef1d434839
Marcello De Bernardi <marcello1234@live.co.uk>
2016-02-27 15:11:12 +0000

    Restarted working on database, different structure

---
+++ app/src/main/java/com/beingbrave/thrifter/thrifter/DatabaseHelper.java
@@ -0,0 +1,21 @@
+package com.beingbrave.thrifter.thrifter;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * For creating instances of databases
+ */
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+
+    /**
+     * Called on initial creation of database
+     * @param database
+     */
+    @Override
+    public void onCreate(SQLiteDatabase database){
+
+    }
+}
