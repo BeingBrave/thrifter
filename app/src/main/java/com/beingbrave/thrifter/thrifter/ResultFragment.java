fb5c5105d164607559ef8bd4e412c2ea64bdc9a3
Melanie Bryant <melaniebryant.mb@gmail.com>
2016-02-27 21:17:01 +0000

    Working tabs

---
+++ app/src/main/java/com/beingbrave/thrifter/thrifter/ResultFragment.java
@@ -0,0 +1,30 @@
+package com.beingbrave.thrifter.thrifter;
+
+        import android.os.Bundle;
+        import android.support.v4.app.Fragment;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+
+        import com.beingbrave.thrifter.thrifter.R;
+
+
+public class ResultFragment extends Fragment{
+
+    public ResultFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.content_result, container, false);
+    }
+
+}
