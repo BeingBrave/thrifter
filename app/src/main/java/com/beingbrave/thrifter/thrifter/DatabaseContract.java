c92f0b8e8b9bd5d515fd29f834277340ecce8fd5
Marcello De Bernardi <marcello1234@live.co.uk>
2016-02-27 15:45:23 +0000

    Adding database stuff

--- app/src/main/java/com/beingbrave/thrifter/thrifter/DatabaseContract.java
+++ app/src/main/java/com/beingbrave/thrifter/thrifter/DatabaseContract.java
@@ -1,5 +1,7 @@
 package com.beingbrave.thrifter.thrifter;
 
+import android.provider.BaseColumns;
+
 /**
  * @author Marcello
  *
@@ -7,4 +9,16 @@ package com.beingbrave.thrifter.thrifter;
  */
 
 public class DatabaseContract {
+
+    // Empty constructor to prevent instantiation
+    public DatabaseContract(){
+    }
+
+    // Table content definitions
+    public static abstract class WhatIsThisShit implements BaseColumns {
+        public static final String TABLE_NAME = "entry";
+        public static final String COLUMN_NAME_ENTRY_ID = "entryID";
+        public static final String COLUMN_NAME_TITLE = "title";
+        public static final String COLUMN_NAME_SUBTITLE = "subtitle";
+    }
 }
