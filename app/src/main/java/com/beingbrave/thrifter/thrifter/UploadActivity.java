181e0d48c5ecedcf8d777ae333640bd275532e62
Christopher <wwwchristopher@msn.com>
2016-02-28 01:35:55 +0000

    FAB working correctly, and upload

--- app/src/main/java/com/beingbrave/thrifter/thrifter/UploadActivity.java
+++ app/src/main/java/com/beingbrave/thrifter/thrifter/UploadActivity.java
@@ -1,10 +1,13 @@
 package com.beingbrave.thrifter.thrifter;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 
 public class UploadActivity extends AppCompatActivity {
@@ -15,6 +18,14 @@ public class UploadActivity extends AppCompatActivity {
         setContentView(R.layout.activity_upload);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowCustomEnabled(true);
+
+        LayoutInflater inflator = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View v = inflator.inflate(R.layout.app_bar_layout, null);
+
+        actionBar.setCustomView(v);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
