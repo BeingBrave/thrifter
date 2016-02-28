e2dc598df0d198ce7b77c48346b9f7e997b1a8de
Melanie Bryant <melaniebryant.mb@gmail.com>
2016-02-28 01:33:15 +0000

    fixed chris icon again

--- app/src/main/java/com/beingbrave/thrifter/thrifter/CardActivity.java
+++ app/src/main/java/com/beingbrave/thrifter/thrifter/CardActivity.java
@@ -1,10 +1,13 @@
 package com.beingbrave.thrifter.thrifter;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 
 public class CardActivity extends AppCompatActivity {
@@ -16,14 +19,22 @@ public class CardActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowCustomEnabled(true);
+
+        LayoutInflater inflator = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View v = inflator.inflate(R.layout.app_bar_layout, null);
+
+        actionBar.setCustomView(v);
+
     }
 
 }
--- app/src/main/java/com/beingbrave/thrifter/thrifter/MainActivity.java
+++ app/src/main/java/com/beingbrave/thrifter/thrifter/MainActivity.java
@@ -93,7 +93,7 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, UploadActivity.class);
+                Intent intent = new Intent(MainActivity.this, CardActivity.class);
                 startActivity(intent);
             }
         });
