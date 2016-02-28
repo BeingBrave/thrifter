45daedc1943abcc98aa417f40b679f522a738ae6
Marcello De Bernardi <marcello1234@live.co.uk>
2016-02-28 04:36:43 +0000

    Deleted google play. Fixed build with multidexing. Going to start working with android location api.

--- app/src/androidTest/java/com/beingbrave/thrifter/thrifter/ApplicationTest.java
+++ app/src/androidTest/java/com/beingbrave/thrifter/thrifter/ApplicationTest.java
@@ -14,7 +14,7 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
 
-        IdentifiersDataSource dataShit = new IdentifiersDataSource(this);
+        IdentifiersDataSource dataShit = new IdentifiersDataSource(getContext());
         dataShit.open();
 
         dataShit.createIdentifier("gayshit");
